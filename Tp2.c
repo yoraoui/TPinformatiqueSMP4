d8eeb2c9d3dea90eef8539d74d9fb093c5126bbd
RAOUI Younès <51831506+yoraoui@users.noreply.github.com>
2022-04-05 13:43:29 +0200

    Update Tp2.c

--- Tp2.c
+++ Tp2.c
@@ -88,3 +88,41 @@ int main(){
     else
         printf("la somme est negative");
 }
+
+
+//Exercice 6
+int main(void){
+    int a, b, c, t;
+    a = 1; b=2; c = 4;
+    float  x1, x2, delta;
+    delta = pow(b, 2) - 4*a*c;
+    if (delta >0)
+        t = 1;
+    else if (delta==0)
+        t = 2;
+    else
+        t = 3;
+    switch (t) {
+        case 1:
+            x1 = (-b -sqrt(delta))/ (2 * a);
+            x2 = (-b +sqrt(delta))/ (2 * a);
+            break;
+        case 2:
+            x1 = -b / (2 * a);
+            x2 = 0;
+            break;
+        case 3:
+            x1 = -b / (2 * a);
+            x2 = sqrt(-delta) / (2 * a);
+            break;
+    }
+    if (t == 1)
+        printf("x1 = \t %.4f , x2 = %.4f", x1, x2);
+    else if (t == 2)
+        printf("x = \t %.4f", x1);
+
+    else if (t == 3)
+        printf("x = \t%f +- j%f", x1, x2);
+}
+
+
